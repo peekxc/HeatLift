f2f12831cc69032fcec4a3aba227d32ab46f3576
peekxc <matt.piekenbrock@gmail.com>
2024-07-05 16:35:07 -0400

    update simple

--- notebooks/simple.py
+++ notebooks/simple.py
@@ -1,25 +1,14 @@
 from more_itertools import factor
 import numpy as np 
+import scipy as sp
 from simplextree import SimplexTree
 from collections import Counter
-
-## example 1 
-authorships = [(0,1)]
-aff_weights = [1]
-
-## example 2
-authorships = [(0,1,2)]
-aff_weights = [0.5]
-
-## exmaple 3
-authorships = [(0,1,2),(1,2,3),(1,3)]
-aff_weights = [0.5, 0.5, 1]
 
 ## Input: non-negative bare affinity weights for each simplex
 ## Output: positive topological weights for each simplex
 ## For collaboration networks, the bare affinity weights are inferred by the number papers co-authored.
 def top_weights(authorships: list, aff_weights: np.ndarray = None):
-  authorships = list(map(tuple, T))
+  authorships = list(map(tuple, authorships))
   paper_counts = Counter(authorships)
 
   st = SimplexTree(authorships)
@@ -42,7 +31,23 @@ def top_weights(authorships: list, aff_weights: np.ndarray = None):
   # for team in filter(lambda a: len(a) == (d+1), authorships):
   #   weights[tuple(team)] += 1
   return st, weights
-  
+
+## example 1 
+authorships = [(0,1)]
+aff_weights = [1]
+top_weights(authorships, aff_weights)
+
+## example 2
+authorships = [(0,1,2)]
+aff_weights = [0.5]
+top_weights(authorships, aff_weights)
+
+## exmaple 3
+authorships = [(0,1,2),(1,2,3),(1,3)]
+aff_weights = [0.5, 0.5, 1]
+top_weights(authorships, aff_weights)
+
+
 node_ids = np.loadtxt("https://raw.githubusercontent.com/DedeBac/WeightedSimplicialComplexes/main/nodelist_ConnComp.txt")
 edges_mn = np.loadtxt("https://raw.githubusercontent.com/DedeBac/WeightedSimplicialComplexes/main/edges_matrix_with_Mn_ConnComp.csv", skiprows=1, delimiter=",")
 triangles_mn = np.loadtxt("https://raw.githubusercontent.com/DedeBac/WeightedSimplicialComplexes/main/tri_matrix_with_Mn_ConnComp.csv", skiprows=1, delimiter=",")
@@ -53,12 +58,10 @@ N = np.arange(len(node_ids))
 E = np.searchsorted(node_ids, edges_mn[:,:2])
 T = np.searchsorted(node_ids, triangles_mn[:,:3])
 
-test_tri = np.searchsorted(node_ids, [14060612200,56909576000,57223418733])
+# test_tri = np.searchsorted(node_ids, [14060612200,56909576000,57223418733])
 
 E_author_card = sp.sparse.coo_array(edges_mn[:,2:])
 T_author_card = sp.sparse.coo_array(triangles_mn[:,3:])
-
-T_author_card.col[T_author_card.row == 0]
 
 from math import factorial, comb
 # from scipy.special import factorial, comb
@@ -74,6 +77,7 @@ for i in range(len(T)):
 T_weights_true = np.loadtxt("https://raw.githubusercontent.com/DedeBac/WeightedSimplicialComplexes/main/trilist_weighted.txt", delimiter=",")
 # np.searchsorted(node_ids, T_weights_true[:,:3])
 assert np.allclose(T_weights, T_weights_true[:,3])
+
 
 ## Triangle weights (from eq. 65, though only restricts to the 2-skeleton)
 c, d = 1.0 / factorial(2), 2
@@ -127,3 +131,26 @@ Counter(e_weights)
 e1 = np.searchsorted(node_ids, [57194375466,57211365771])
 # 268, 323 => 0.2
 st.cofaces([268,323])
+
+
+def read_hgf(file: str):
+  with open(file, "r") as file:
+    lines = [line.rstrip() for line in file]
+  n, m = map(int, lines[0].split(' '))
+  return (n,m), [[int(i.split("=")[0]) for i in L.split(" ")] for L in lines[1:]]
+
+(n,m), hyperedges = read_hgf("data/algebra.hgf")
+
+from collections import Counter
+## Stores, for each hyper-edge h, a dict (k,v) representing h participating in v papers having k total authors 
+collab_sizes = []
+for i, he in enumerate(hyperedges):
+  C1 = [len(h) for h in hyperedges[:i] if np.all(np.isin(he, h))]
+  C2 = [len(h) for h in hyperedges[(i+1):] if np.all(np.isin(he, h))]
+  collab_sizes.append(dict(Counter(C1 + C2)))
+  print(i)
+
+from math import comb
+np.sum([comb(len(h),2) for h in hyperedges])
+np.sum([comb(len(h),3) for h in hyperedges])
+hyperedges[0]
