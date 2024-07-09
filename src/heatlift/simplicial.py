3f722d3a478d2d1bbbe19fd69e395e362b7c676f
peekxc <matt.piekenbrock@gmail.com>
2024-07-08 22:34:24 -0400

    minor changes to the simplicial weight mappig

--- src/heatlift/simplicial.py
+++ src/heatlift/simplicial.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools as it
+from typing import Generator, Sized
 from collections import Counter, defaultdict
 from math import comb, factorial
 
@@ -10,33 +11,38 @@ BASE_WEIGHTS = {
   3: {0: 1, 1: 1/3, 2: 1/6, 3: 1/6} # 3-simplex
 }
 
+def faces(simplex: tuple, proper: bool = False) -> Generator:
+  max_dim = len(simplex) - int(proper)
+  faces_gen = (it.combinations(simplex, d) for d in range(1, max_dim+1))
+  yield from it.chain(*faces_gen)
+
+def dim(simplex: Sized) -> int:
+  return len(simplex) - 1
+
 def weighted_simplex(sigma: tuple) -> dict:
   """Constructs a dictionary mapping faces of 'sigma' to their base topological weights.
   
   The resulting simplex->weight mapping obeys the property that every p-simplex's weight 
   is given by the sum of cofacet weights. Moreover, this relation is preserved under 
   composition with weight maps constructed from other non-face simplices.
-
-  For example, to encode a hypergraph (given its hyperedges) into a weighted simplicial complex with positive weights:
-  
   """
-  dim = lambda s: len(s) - 1
-  faces = lambda s: it.chain(*[it.combinations(sigma,d) for d in range(1, len(s))])
   weights = defaultdict(float)
-  weights[tuple(sigma)] = 1 / factorial(dim(sigma))
   base_weights = BASE_WEIGHTS[dim(sigma)]
-  for f in faces(sigma):
+  for f in faces(sigma, proper=True):
     weights[f] += base_weights[dim(f)]
+  weights[tuple(sigma)] = 1 / factorial(dim(sigma))
   return Counter(weights)
 
 ## Ensure the cofacet relations hold; this should hold for all simplices with base weights / "topological weight"
 ## However this breaks as soon as the affinity weights are added 
-def _cofacet_relation(S, weights, verbose: bool = False) -> bool:
+def _cofacet_relation(S, verbose: bool = False) -> bool:
+  from simplextree import SimplexTree
+  st = SimplexTree(S.keys())
   relation_holds: bool = True
-  for s in S:
-    s_weight = weights[s]
-    s_cofacets = cofacets(s)
-    c_weight = np.sum([weights[c] for c in s_cofacets])
+  for s in st:
+    s_weight = S[s]
+    s_cofacets = [c for c in st.cofaces(s) if dim(c) == dim(s) + 1]
+    c_weight = np.sum([S[c] for c in s_cofacets])
     relation_holds &= np.isclose(s_weight, c_weight) or len(s_cofacets) == 0
     if verbose and not relation_holds: 
       print(f"simplex {s} weight {s_weight:.3f} != cofacet weight {c_weight:.3f}")
